10d75abd0cf2b535947f17b72788158a643ed3f6
yuanmenglong <yuanmenglong@didichuxing.com>
2018-09-10 22:15:18 +0800

    * 1. init project

---
+++ Objc/Objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Objc
+//
+//  Created by yuanmenglong on 2018/9/10.
+//  Copyright © 2018年 yuanmenglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objc/Objc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Objc
+//
+//  Created by yuanmenglong on 2018/9/10.
+//  Copyright © 2018年 yuanmenglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
